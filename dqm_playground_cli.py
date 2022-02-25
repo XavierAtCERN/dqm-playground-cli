52fa0115c1bb3bcf84c1cee71b8f50e453a14a6b
Xavier Coubez <xavier.coubez@cern.ch>
2022-02-25 17:12:27 +0100

    Solving build failing

--- dqm_playground_cli.py
+++ dqm_playground_cli.py
@@ -37,7 +37,7 @@ class Config(object):
     def __init__(self):
         self.verbose = False
 
-pass_config = click.make_pass_decorator(Config)
+pass_config = click.make_pass_decorator(Config, ensure=True)
 
 # cli
 @click.group()
